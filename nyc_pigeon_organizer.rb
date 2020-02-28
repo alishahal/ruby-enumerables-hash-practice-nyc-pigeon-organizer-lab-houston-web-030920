33dfacb383293790ae82873e47fad9e41b77b873
Karim Macknojia <alishahal@msn.com>
2020-02-28 20:04:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |key, value| 
-     value.each do |new_value, names|
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
       names.each do |name|
         
         if !new_hash[name]
@@ -14,9 +14,8 @@ def nyc_pigeon_organizer(data)
 
         new_hash[name][key] << new_value.to_s
 
-    end
+      end
     end
   end
   new_hash
-  
 end
